ad257249ec3aa3f9f987a70c4e38fe45eabdcc1f
ltw070 <ltw070@naver.com>
2025-03-27 14:50:11 +0900

    p26 수정 커밋

--- PrimeFactors/prime_factors_test.cpp
+++ PrimeFactors/prime_factors_test.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+TEST(PrimeFactors, PrimTest) {
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+}
+
 int main() {
 	testing::InitGoogleTest();
 	return RUN_ALL_TESTS();
