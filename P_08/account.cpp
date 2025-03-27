fe343c5bbbd372e0be4bd8bde3eec9f7e5913155
ltw070 <ltw070@naver.com>
2025-03-27 11:02:59 +0900

    second

--- P_08/account.cpp
+++ P_08/account.cpp
@@ -1,10 +1,4 @@
 class Account {
 public:
-	int getBalance() {
-		return balance;
-	}
-
-private:
-	int balance = 10000;
 
 };
--- P_08/main.cpp
+++ P_08/main.cpp
@@ -1,11 +1,6 @@
 #include"gmock/gmock.h"
 #include "account.cpp"
 
-TEST(Fibotest, OrderTest) {
-	Account account;
-	int ret = account.getBalance();
-		EXPECT_EQ(10000, ret);
-}
 
 int main() {
 	::testing::InitGoogleMock();
