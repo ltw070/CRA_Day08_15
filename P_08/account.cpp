dd34c96fbc6160e210a46f8fe74b7f61af816d55
ltw070 <ltw070@naver.com>
2025-03-27 11:35:26 +0900

    complete 1st

--- P_08/account.cpp
+++ P_08/account.cpp
@@ -1,4 +1,22 @@
 class Account {
 public:
+	explicit Account(int balance)
+		: balance(balance) {
 
+	}
+
+	int getBalance() {
+		return balance;
+	}
+
+	void deposit(int money) {
+		balance += money;
+	}
+
+	void withdraw(int money) {
+		balance -= money;
+	}
+
+private:
+	int balance;
 };
--- P_08/main.cpp
+++ P_08/main.cpp
@@ -1,6 +1,29 @@
 #include"gmock/gmock.h"
 #include "account.cpp"
 
+using namespace std;
+
+class AccountFixture : public testing::Test {
+public:
+	Account account{ 10000 };
+};
+
+TEST_F(AccountFixture, CreateAccountInit10000won) {
+	//Account account(10000);
+	EXPECT_EQ(account.getBalance(), 10000);
+}
+
+TEST_F(AccountFixture, Deposit) {
+	//Account account(10000);
+	account.deposit(500);
+	EXPECT_EQ(account.getBalance(), 10500);
+}
+
+TEST_F(AccountFixture, Withdraw) {
+	//Account account(10000);
+	account.withdraw(600);
+	EXPECT_EQ(account.getBalance(), 9400);
+}
 
 int main() {
 	::testing::InitGoogleMock();
